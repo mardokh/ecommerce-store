d50833cebc81dcfd3daed7666a6bb79f316be388
mardokh <ahoura.mardokh@gmail.com>
2025-02-16 14:26:44 +0100

    Modify error msg on images erros handlers

--- back/middlewares/multerConfig.js
+++ back/middlewares/multerConfig.js
@@ -10,7 +10,7 @@ const storage = multer.memoryStorage();
 // File filter function
 const fileFilter = (req, file, callback) => {
     if (!validMimeTypes.includes(file.mimetype)) {
-        return callback(new Error('Invalid file type. Only PNG, JPG, and JPEG are allowed.'));
+        return callback(new Error('Format invalide, (png, jpg, jpeg) seulement'));
     }
     callback(null, true);
 };
@@ -30,12 +30,12 @@ const upload = (req, res, next) => {
     multerUpload(req, res, (err) => {
         if (err) {
             if (err.code === 'LIMIT_FILE_SIZE') {
-                return res.status(400).json({ error: "File too large. Maximum size is 2MB." });
+                return res.status(400).json({ error: "Votre image ne doit pas depassé 2MB" });
             }
             if (err.code === 'LIMIT_UNEXPECTED_FILE') {
-                return res.status(400).json({ error: "Too many images uploaded. Maximum 10 allowed for 'images' and 1 for 'image'." });
+                return res.status(400).json({ error: "Nombre d'image depassé (maximum 10 images)" });
             }
-            return res.status(400).json({ error: err.message || "Something went wrong during file upload." });
+            return res.status(400).json({ error: err.message || "Something went wrong during file upload" });
         }
         next();
     });
