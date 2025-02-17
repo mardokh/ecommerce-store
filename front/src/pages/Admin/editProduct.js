bedface2d733cdd904e8440d0d4c7c8cef565861
mardokh <ahoura.mardokh@gmail.com>
2025-02-17 14:39:45 +0100

    Update editProduct

--- front/src/pages/Admin/editProduct.js
+++ front/src/pages/Admin/editProduct.js
@@ -23,6 +23,7 @@ const EditProduct = () => {
     const [priceError, setPriceError] = useState("")
     const [imageError,setImageError] = useState("")
     const [imagesError,setImagesError] = useState("")
+    const [onImageDelete, setOnImageDelete] = useState(false)
 
 
     // REFERENCE //
@@ -131,6 +132,14 @@ const EditProduct = () => {
     // SECONDARYS IMAGES DELETE //
     const deleteImage = async (index, imageId) => {
         try {
+            if (imageId) {
+                setOnLoader(true)
+                setLoader(true)
+                await productService.deleteSecondaryImage(imageId)
+                setOnImageDelete(true)
+                setLoader(false)
+                setTimeout(() => setOnLoader(false), 2000)
+            }
             setProduct(prevImages => {
                 const updatedImages = [...prevImages.images]
                 updatedImages.splice(index, 1)
@@ -140,10 +149,7 @@ const EditProduct = () => {
                 const updatedUrls = [...prevUrls]
                 updatedUrls.splice(index, 1)
                 return updatedUrls
-            })            
-            if (imageId) {
-                await productService.deleteSecondaryImage(imageId)
-            }
+            })
         }
         catch (err) {
             console.error('Error on deleteImage : ', err)
@@ -238,7 +244,7 @@ const EditProduct = () => {
                             <svg className="edit_product_success_icon" viewBox="0 0 20 20">
                                 <path d="M10.219,1.688c-4.471,0-8.094,3.623-8.094,8.094s3.623,8.094,8.094,8.094s8.094-3.623,8.094-8.094S14.689,1.688,10.219,1.688 M10.219,17.022c-3.994,0-7.242-3.247-7.242-7.241c0-3.994,3.248-7.242,7.242-7.242c3.994,0,7.241,3.248,7.241,7.242C17.46,13.775,14.213,17.022,10.219,17.022 M15.099,7.03c-0.167-0.167-0.438-0.167-0.604,0.002L9.062,12.48l-2.269-2.277c-0.166-0.167-0.437-0.167-0.603,0c-0.166,0.166-0.168,0.437-0.002,0.603l2.573,2.578c0.079,0.08,0.188,0.125,0.3,0.125s0.222-0.045,0.303-0.125l5.736-5.751C15.268,7.466,15.265,7.196,15.099,7.03"></path>
                             </svg>
-                            <p>Modification effecuter avec succes</p>
+                            <p>{onImageDelete ? "Image supprimer avec succes" : "Modification effecuter avec succes"}</p>
                         </div>
                         } 
                     </div>
