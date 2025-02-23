6afee749436218439347ab413a3f27a4986a4aad
mardokh <ahoura.mardokh@gmail.com>
2025-02-23 21:00:45 +0100

    create route for getCategoriesNames

--- back/routes/products.js
+++ back/routes/products.js
@@ -13,27 +13,23 @@ let router = express.Router()
 // GET PRODUCTS //
 router.get('', controller.getAllProducts)
 
-
 // GET PRODUCT //
 router.get('/:id', validateGetProduct, controller.getOneProduct)
 
+// GET CATEGORIES NAMES //
+router.get('', controller.getCategoriesNames)
 
 // CREATE PRODUCT //
 router.put('/create', upload, validateCreateProduct, controller.createProduct)
 
-
-
 // UPDATE PRODUCT //
 router.patch('/update', upload, validateUpdateProduct, controller.updateProduct)
-
 
 // DELETE PRODUCT //
 router.delete('/delete/:id', checkTokenMIddleware, validateDeleteProduct, controller.deleteProduct)
 
-
 // DELETE SECONDARY IMAGE //
 router.delete('/secondaryimage/:id', checkTokenMIddleware, validateDeleteSecondaryImage, controller.deleteSecondaryImage)
-
 
 
 // EXPORTS //
